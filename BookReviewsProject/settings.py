72ad43411003381b16b6de4476a7059c460803ac
kunxin-chor <chorkunxin@yahoo.com>
2020-08-25 06:55:53 +0000

    Added templates

--- BookReviewsProject/settings.py
+++ BookReviewsProject/settings.py
@@ -61,7 +61,10 @@ ROOT_URLCONF = 'BookReviewsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
