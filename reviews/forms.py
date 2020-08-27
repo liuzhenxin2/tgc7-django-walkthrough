5d82de02c5569e62b291d91d786f8bbf4119421e
kunxin-chor <chorkunxin@yahoo.com>
2020-08-27 07:11:07 +0000

    Added search for reviews

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
 from .models import Review
+from django.contrib.auth.models import User
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         exclude = ('book', 'author')
+
+
+class SearchReviewForm(forms.Form):
+    review_title = forms.CharField(required=False)
+    book_title = forms.CharField(required=False)
+    author = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, reverse, redirect
-from .models import Review
-from .forms import ReviewForm
-from books.models import Book
+from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from books.models import Book
+from .models import Review
+from .forms import ReviewForm, SearchReviewForm
+
 # Create your views here.
 
 
 def index(request):
-    all_reviews = Review.objects.all()
-    return render(request, 'show_reviews.template.html', {
-        'all_reviews': all_reviews
+
+    form = SearchReviewForm(request.GET)
+
+    # create the always true query
+    # the following means `WHERE 1` in SQL
+    query = ~Q(pk__in=[])
+
+    if request.GET:
+        # to search by the review title
+        if 'review_title' in request.GET and request.GET['review_title']:
+            review_title = request.GET['review_title']
+            query = query & Q(title__icontains=review_title)
+
+        # to search by book title
+        if 'book_title' in request.GET and request.GET['book_title']:
+            query = query & Q(book__title__icontains=request.GET['book_title'])
+        
+        # search by author
+        if 'author' in request.GET and request.GET['author']:
+            query = query & Q(author=request.GET['author'])
+
+    all_reviews = Review.objects.filter(query)
+    return render(request, 'reviews/show_reviews.template.html', {
+        'all_reviews': all_reviews,
+        'form': form
     })
 
 
